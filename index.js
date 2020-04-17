dab6c454fce0cae4ad897952c3bb0aa5ac330ffe
Michael De La Rosa <56710845+m-delarosa@users.noreply.github.com>
2020-04-17 11:47:45 -0600

    Add frontend add report modal.

--- index.js
+++ index.js
@@ -2,13 +2,22 @@ document.addEventListener("DOMContentLoaded", () => {
   console.log("%cDOM Content Loaded and Parsed!", "color: magenta")
 
   const $loginForm = document.querySelector(".login-form")
+  const $registerForm = document.querySelector(".register-form")
+
   const $profileLink = document.querySelector("#profile")
   const $loginLink = document.getElementById("login")
+
   const $closeLoginButton = document.querySelector(".close")
   const $closeRegisterButton = document.querySelector(".close-register")
   const $registerLink = document.getElementById("register")
 
-  
+  const $closeReportButton = document.querySelector(".close-report")
+
+  //   const $editButtons = document.querySelectorAll(".edit-button")
+  //   $editButtons.forEach( (button) => button.addEventListener("click", () => {
+  //     console.log("edit button clicked")
+  // }))
+
   $loginLink.addEventListener("click", () => {
     event.preventDefault()
     document.querySelector(".bg-modal").style.display = "flex"
@@ -21,6 +30,7 @@ document.addEventListener("DOMContentLoaded", () => {
     document.querySelector(".bg-modal-register").style.display = "flex"
   })
   $closeRegisterButton.addEventListener("click", closeRegisterModal)
+  $registerForm.addEventListener("submit", handleRegisteration)
 
   fetch("http://localhost:3000/trails")
     .then((response) => response.json())
@@ -43,6 +53,10 @@ document.addEventListener("DOMContentLoaded", () => {
     checkToken()
   }
 
+  function closeReportModal() {
+    document.querySelector(".bg-modal-add-report").style.display = "none"
+  }
+
   function addTrail(trail) {
     const $trailsTable = document.querySelector("#trails-table")
     const $tableRow = document.createElement("tr")
@@ -53,7 +67,12 @@ document.addEventListener("DOMContentLoaded", () => {
     addDate(trail, $tableRow)
 
     const $addReport = document.createElement("td")
-    $addReport.innerHTML = '<a href="#edit">Edit</a>'
+    $addReport.innerHTML = '<a href="#edit" class="edit-button">Edit</a>'
+    $addReport.addEventListener("click", () => {
+      event.preventDefault()
+      document.querySelector(".bg-modal-add-report").style.display = "flex"
+    })
+    $closeReportButton.addEventListener("click", closeReportModal)
     $tableRow.append($addReport)
   }
 
@@ -122,9 +141,34 @@ document.addEventListener("DOMContentLoaded", () => {
       })
   }
 
+  function handleRegisteration(event) {
+    event.preventDefault()
+    const registrationFormData = new FormData(event.target)
+    const name = registrationFormData.get("name")
+    const username = registrationFormData.get("username")
+    const email = registrationFormData.get("email")
+    const password = registrationFormData.get("password")
+    const registrationBody = { user: { name, username, email, password } }
+
+    fetch("http://localhost:3000/users", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(registrationBody),
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        // console.log(result.token)
+        // localStorage.setItem("token", result.token)
+        handleRegistrationUserResponse(result, $registerForm)
+      })
+  }
+
   function handleLoginUserResponse(response, form) {
     const $errorMessage = document.getElementById("response-error-message")
-    
+
     if ($errorMessage) {
       form.removeChild($errorMessage)
     }
@@ -136,17 +180,46 @@ document.addEventListener("DOMContentLoaded", () => {
       const $responseMessage = document.createElement("li")
       $responseMessage.innerText = `Welcome back ${response.user.name}. ${response.message}`
       form.append($responseMessage)
-    //   $responseList.append($responseMessage)
+      //   $responseList.append($responseMessage)
     } else {
       const $errorMessage = document.createElement("li")
-      $errorMessage.id = ("response-error-message")
+      $errorMessage.id = "response-error-message"
       $errorMessage.innerText = "Please verify login credentials and try again."
       form.append($errorMessage)
-    //   $responseList.append($responseMessage)
+      //   $responseList.append($responseMessage)
     }
-
-    
-
   }
 
+  function handleRegistrationUserResponse(response, form) {
+    const $errorMessage = document.getElementById("response-error-message")
+
+    if ($errorMessage) {
+      form.removeChild($errorMessage)
+    }
+
+    if (response.user) {
+      console.log("User created")
+      //   localStorage.setItem("token", response.token)
+      form.reset()
+      const $responseMessage = document.createElement("li")
+      $responseMessage.innerText = `Welcome ${response.user.name}, your user account was created sucessfully.\n Try logging in.`
+      form.append($responseMessage)
+      //   $responseList.append($responseMessage)
+    } else {
+      const responseKeys = Object.keys(response)
+      const $errorMessage = document.createElement("ul")
+      $errorMessage.id = "response-error-message"
+      $errorMessage.innerText =
+        "We were unable to create your account due to the following:"
+
+      responseKeys.forEach((responseKey) => {
+        const li = document.createElement("li")
+        li.innerText = response[responseKey]
+        $errorMessage.append(li)
+      })
+
+      form.append($errorMessage)
+      //   $responseList.append($responseMessage)
+    }
+  }
 })
